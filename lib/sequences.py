f2780406685715baf20840b5597ac83b82c07e35
Ivy <ivyjkemboi@gmail.com>
2024-07-13 23:59:31 +0300

    when is fibonacci ever useful?

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+   
+    if length == 0:
+        print (list())
+
+    elif length == 1:
+        print ([0])
+
+    elif length >= 2:
+        new_list = [0, 1]
+
+        while len(new_list) < length:
+            new_var = new_list[-1] + new_list[-2]
+            new_list.append(new_var)
+            
+        print (new_list)
+
+    else:
+        print ('out of range')
+          
+
+    
